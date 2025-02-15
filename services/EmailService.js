d664939279dfb0b7810166f2e810994206228029
AnasMohammad4321 <mohammadanas702@gmail.com>
2025-02-15 11:17:40 +0500

    Fix SMTP configuration for GitHub Actions

--- services/EmailService.js
+++ services/EmailService.js
@@ -7,7 +7,7 @@ class EmailService {
       service: process.env.SMTP_HOST,
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
